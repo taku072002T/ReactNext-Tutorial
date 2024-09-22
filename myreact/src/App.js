53b6ff9116f02db188c4771f9ea2785df20c7932
taku072002T <taku07200@gmail.com>
2024-09-22 22:15:19 +0900

    Added Learning Management memo

---
+++ myreact/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const learnings = ["9/22:自分なりのWeb開発チュートリアルを執筆中。Firebaseについての勉強もした。大学関係では、物理学の勉強をした。AIを提携して、WebカメラをつかったWebアプリ開発をしてみたい。Streamlitで作れるかな",""]
+  var paper = learnings.map(item => <li>{item}</li>)
+  return (
+    <div>
+      <h1>プロフィール</h1>
+      <p>たくみ</p>
+      <a>大学生エンジニア。フルスタック開発者を目指してスキルアップ中。セキュリティら辺の学習が甘いので、CTFとかやってみたいんです。最新の情報を取り入れる体制を整えてる最中。</a>
+      <p>2024 〇〇大学進学</p>
+      <p>〇〇大学在籍中</p>
+      <h1>学習中のスキル</h1>
+      <ul>
+      <li>React</li>
+      <li>Typescript</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>TAILWINDCSS</li>
+      <li>Astro</li>
+      </ul>
+      <h1>学習したいスキル</h1>
+      <ul>
+        <li>Blenderでモデル制作</li>
+        <li>C#でアプリケーション開発</li>
+        <li>アーキテクチャ</li>
+        <li>競技プログラミング</li>
+        <li>CTF</li>
+        <li>その他</li>
+      </ul>
+      <h1>学習管理</h1>
+      <ul>
+        {paper}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
